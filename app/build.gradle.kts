f0dc7194c4108d8993d1675f51053713d419bb0d
stevannprb <stevan.purbagal@gmail.com>
2026-03-04 12:56:57 +0700

    MENAMBAHKAN AKTIVITAS LARI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,16 +4,12 @@ plugins {
 
 android {
     namespace = "com.upn.fitrun"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.upn.fitrun"
         minSdk = 27
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
